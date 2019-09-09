9dcd5194f3d25196a4170f714766bcddadf1d792
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2019-09-09 17:26:03 +0900

    Add gunicorn and change port

--- api.py
+++ api.py
@@ -1,3 +1,11 @@
+import os
+import io
+import time
+import numpy as np
+# import cv2
+# import dlib
+from flask import render_template, redirect, url_for, send_from_directory, session
+from werkzeug import secure_filename
 from gensim.models import Word2Vec
 from flask import Flask, jsonify, request
 import nltk
@@ -11,4 +19,4 @@ def word2vec():
     return jsonify(model.most_similar(positive=[w], topn=10))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
