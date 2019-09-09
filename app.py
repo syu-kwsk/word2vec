2d19ab13beba415336f3cb25b184880b16a4b202
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2019-09-09 17:52:17 +0900

    Delete hello world

--- app.py
+++ app.py
@@ -10,18 +10,18 @@ from gensim.models import Word2Vec
 from flask import Flask, jsonify, request
 import nltk
 
-app = Flask(__name__)
-@app.route('/')
-def index():
-    return 'Hello World!'
-
 #app = Flask(__name__)
-#app.config['JSON_AS_ASCII'] = False
 #@app.route('/')
-#def word2vec():
-#    model = Word2Vec.load("./wiki.model")
-#    w = request.args['word']
-#    return jsonify(model.most_similar(positive=[w], topn=10))
+#def index():
+#    return 'Hello World!'
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+@app.route('/')
+def word2vec():
+    model = Word2Vec.load("./wiki.model")
+    w = request.args['word']
+    return jsonify(model.most_similar(positive=[w], topn=10))
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
